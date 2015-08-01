c9899a2066ebbe93c23bcb15e5d1eff8ee93be60
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-07-31 23:00:47 -0500

    refact: uses DDRB5 as output and DDRB4 as input

--- firmware/main.c
+++ firmware/main.c
@@ -7,18 +7,17 @@
 
 int main(void)
 {
-	DDRB = 0b00100000; //Salida DDRB5
-	DDRC = 0b00000000; //ENTRADA DDRC
+	DDRB = 0x20;	//0b00100000 Output DDRB5
 
-	PORTB = 0;
+	PORTB = 0x10;//0b00010000 Input DDRB4
 
    while(1) {
 
-   	if(!(PINC & 0x20)) {
-   		PORTB = 0x20;
+   	if(!(PINB & 0x10)) {
+   		PORTB |= 0x20;
    	}
    	else {
-   		PORTB = 0;	
+   		PORTB &= 0xDF; //0b 1101 1111
    	}
    }
 
